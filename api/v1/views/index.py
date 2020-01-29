5340d8d2571fcd371ccdd18bdebceb1147f0e57b
edward0rtiz <931@holbertonschool.com>
2020-01-29 17:14:45 -0500

    [fix] notation and indentation at status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" API test stats"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -14,9 +14,11 @@ def status():
 @app_views.route('/stats')
 def stats():
     """stats"""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        })
