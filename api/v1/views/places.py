3ae14419f67446a9be9515816bed03bc85084680
edward0rtiz <931@holbertonschool.com>
2020-01-29 19:14:28 -0500

    [fix] refactor by rmeoving .pop cascade at flask

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,27 +7,42 @@ from models import storage
 from models.place import Place
 
 
-@app_views.route('/places/<place_id>', methods=['GET', 'PUT', 'DELETE'])
+@app_views.route('/places/<place_id>')
+def get_places(place_id=None):
+    places = storage.get("Place", place_id)
+    if places is None:
+        abort(404)
+    else:
+        return jsonify(place.to_dict())
+
+
+@app_views.route('/cities/<city_id>/places')
+def all_place(city_id=None):
+    """CREATE places."""
+    list_places = []
+    city_obj = storage.get("City", city_id)
+    if city_obj is None:
+        abort(404)
+    else:
+        for place_city in city_obj:
+            list_places.append(place_city.to_dict())
+        return jsonify(list_places)
+
+
+@app_views.route('/places/<place_id>', methods=['PUT', 'DELETE'])
 def place(place_id=None):
-    """ GET, 'PUT, 'DELETE'"""
+    """'PUT, 'DELETE' methods"""
     obj_place = storage.get("Place", place_id)
     if obj_place is None:
         abort(404)
-
     if request.method == 'DELETE':
         obj_place.delete()
         storage.save()
         return jsonify({}), 200
     if request.method == 'PUT':
-        do_put = request.get_json()
-        if not do_put:
+        if not request.is_json:
             abort(400, "Not a JSON")
-
-        obj_place.pop('id', None)
-        obj_place.pop('created_at', None)
-        obj_place.pop('updated_at', None)
-        obj_place.pop('user_id', None)
-        obj_place.pop('city_id', None)
+        do_put = request.get_json()
         for k, v in do_out.items():
             if (k is not "id" and k is not "created_at" and
                     k is not "updated_at" and k is not "user_id" and
@@ -38,46 +53,23 @@ def place(place_id=None):
 
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'])
-def place_post(city_id=None):
+def place_post(city_id):
     """POST method"""
     get_city = storage.get("City", city_id)
     if get_city is None:
         abort(404)
-
     do_post = request.get_json()
-    if do_post is not request.is_json:
+    if do_post not request.is_json:
         abort(400, "Not a JSON")
-    user_id = do_post.get("user_id", None)
-    if user_id is not request.is_json:
+    if 'user_id' not request.is_json:
         abort(400, "Missing user_id")
-    name = do_post.get("name", None)
-    if name is not request.is_json:
+    if 'name' not request.is_json:
         abort(400, "Missing name")
-
-    do_post.pop('id', None)
-    do_post.pop('created_at', None)
-    do_post.pop('updated_at', None)
-    do_post.pop({'city_id': city_id})
-
+    new_usr = do_post.get("user_id")
     if storage.get("User", user_id) is None:
         abort(404)
-    for place in storage.all("Place").values():
-        if place.name == name and place.user_id == user_id:
-            for k, v in do_post.items():
-                setattr(place, k, v)
-            place.save()
-            return jsonify(place.to_dict()), 200
     new_place = Place(**do_post)
-    new_place.save()
-    return jsonify(new_place.to_dict()), 201
-
-
-@app_views.route('/cities/<city_id>/places')
-def all_place(city_id=None):
-    """CREATE places."""
-    city_obj = storage.get("City", city_id)
-    if city_obj is None:
-        abort(404)
-    all_p = storage.all("Place").values()
-    places = [place.to_dict() for place in all_p if place.city_id == city_id]
-    return jsonify(places)
+    setattr(new_place, "city_id", city_id)
+    storage.do_post(new_place)
+    storage.save()
+    return jsonify(place.to_dict()), 201
