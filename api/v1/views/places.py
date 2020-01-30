194d56a2bad8d366f135ed5e38bcf9c912ee8946
edward0rtiz <931@holbertonschool.com>
2020-01-29 19:32:11 -0500

    [fix] is not funtion

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -40,32 +40,13 @@ def place(place_id=None):
         obj_place.delete()
         storage.save()
         return jsonify({}), 200
-    if request.method == 'PUT':
-        if not request.is_json:
-            abort(400, "Not a JSON")
-        do_put = request.get_json()
-        for k, v in do_put.items():
-            if (k is not "id" and k is not "created_at" and
-                    k is not "updated_at" and k is not "user_id" and
-                    k is not "city_id"):
-                setattr(obj_place, k, v)
-        obj_place.save()
-        return jsonify(obj_place.to_dict()), 200
-
-
-@app_views.route('/cities/<city_id>/places', methods=['POST'])
-def place_post(city_id):
-    """POST method"""
-    get_city = storage.get("City", city_id)
-    if get_city is None:
-        abort(404)
-    do_post = request.get_json()
-    if do_post not request.is_json:
+    if not request.is_json:
         abort(400, "Not a JSON")
-    if 'user_id' not request.is_json:
+    if 'user_id' not in request.is_json:
         abort(400, "Missing user_id")
-    if 'name' not request.is_json:
+    if 'name' not in request.is_json:
         abort(400, "Missing name")
+    do_post = request.get_json()
     new_usr = do_post.get("user_id")
     if storage.get("User", user_id) is None:
         abort(404)
