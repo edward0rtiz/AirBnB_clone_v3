7424d410b62ff610bacd9755fcc227037cea6dbf
edward0rtiz <931@holbertonschool.com>
2020-01-30 18:15:16 -0500

    [fix] GET method return json of list at amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,21 +8,20 @@ from models.amenity import Amenity
 from os import getenv
 import sqlalchemy
 
-db_storage = (getenv("HBNB_TYPE_STORAGE"), "json_file")
+db = (getenv("HBNB_TYPE_STORAGE"), "json_file")
 
 
 @app_views.route('/places/<place_id>/amenities')
-def place_amenities(place=None):
+def place_amenities(place_id=None):
     """GET method for amenities en place"""
     list_amenities = []
     obj_place_am = storage.get("Place", place_id)
     if obj_place_am is None:
         abort(404)
-    else:
-        if db == "db":
-            for amenity in obj_place_am.list_amenities:
-                list_amenities.append(amenity.to_dict())
-            return jsonify(obj_place_am.list_amenities())
+    amenities = obj_place_am.amenities
+    for amenity in amenities:
+        list_amenities.append(amenity.to_dict())
+    return jsonify(list_amenities)
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
@@ -36,7 +35,6 @@ def amenity_review_delete(place_id=None, amenity_id=None):
     list = [item.id for item in obj_place.amenities]
     if obj_amenity.id not in list:
         abort(404)
-
     if db == 'db':
         obj_place.amenities.remove(obj_amenity)
     else:
@@ -50,12 +48,13 @@ def amenity_place(place_id, amenity_id):
     """POST method for amenities en place"""
     obj_place = storage.get("Place", place_id)
     obj_amenity = storage.get("Amenity", amenity_id)
-    if obj_place is None or obj_amenity is None:
+    if obj_place is None:
+        abort(404)
+    if obj_amenity is None:
         abort(404)
     list = [item.id for item in obj_place.amenities]
     if obj_amenity.id in list:
-        return jsonify(obj_amenity.to_dict()), 200
-
+        return (jsonify(obj_amenity.to_dict()), 200)
     if db is 'db':
         obj_place.amenities.append(obj_amenity)
     else:
