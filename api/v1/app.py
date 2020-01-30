c7b4992f486e9a8353a3b223cb9b1f514c4a3814
edward0rtiz <931@holbertonschool.com>
2020-01-30 09:11:37 -0500

    [fix] add CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,14 +4,16 @@ import os
 from models import storage
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
+db = os.environ.get('HBNB_TYPE_STORAGE', 'json_file')
 host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
 port = os.environ.get('HBNB_API_PORT', '5000')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close(cls):
