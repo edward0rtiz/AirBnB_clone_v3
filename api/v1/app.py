be7523f0ab8ca244e2ad9e4e3965ee01d39ca764
Sergio Zamudio <szamu5@hotmail.com>
2020-01-30 21:26:10 -0500

    [REFACTOR] works again

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,18 +5,17 @@ from models import storage
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from flask_cors import CORS
-from flasgger import Swagger
+"""from flasgger import Swagger"""
 
 
 app = Flask(__name__)
-swagger = Swagger(app)
+"""swagger = Swagger(app)"""
 db = os.environ.get('HBNB_TYPE_STORAGE', 'json_file')
 host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
 port = os.environ.get('HBNB_API_PORT', '5000')
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
